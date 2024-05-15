9931345a239b786b5b1d3827fb5f1305f810e0a0
Ayush Goel <ayush.goel2427@gmail.com>
2024-05-15 10:26:38 -0400

    Added dataset handlers

---
+++ dataset/dataloader.py
@@ -0,0 +1,50 @@
+import torch
+from torch.utils.data import DataLoader, random_split
+from dataset.configuration_manager import ConfigManager
+
+class DataLoaderWrapper:
+    
+    def __init__(self, dataset, config_path='dataloader_config.yaml'):
+        self.dataset = dataset
+        dataloader_config = ConfigManager.fetch_dataloader_config()
+
+        self.batch_size = dataloader_config['batch_size']
+        self.train_split = dataloader_config['train_split']
+        self.val_split = dataloader_config['val_split']
+        self.shuffle = dataloader_config['shuffle']
+        self.num_workers = dataloader_config['num_workers']
+
+        self.train_loader, self.val_loader, self.test_loader = self.create_loaders()
+    
+    @staticmethod
+    def calculate_split_sizes(dataset_size, train_split, val_split):
+        train_size = int(train_split * dataset_size)
+        val_size = int(val_split * dataset_size)
+        test_size = dataset_size - train_size - val_size
+        return train_size, val_size, test_size
+
+    def create_loaders(self):
+        total_size = len(self.dataset)
+        train_size, val_size, test_size = self.calculate_split_sizes(total_size, self.train_split, self.val_split)
+
+        if self.shuffle:
+            train_dataset, val_dataset, test_dataset = random_split(self.dataset, [train_size, val_size, test_size])
+        else:
+            train_dataset = torch.utils.data.Subset(self.dataset, range(0, train_size))
+            val_dataset = torch.utils.data.Subset(self.dataset, range(train_size, train_size + val_size))
+            test_dataset = torch.utils.data.Subset(self.dataset, range(train_size + val_size, total_size))
+
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=self.num_workers)
+        val_loader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+        test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+
+        return train_loader, val_loader, test_loader
+
+    def get_train_loader(self):
+        return self.train_loader
+
+    def get_val_loader(self):
+        return self.val_loader
+
+    def get_test_loader(self):
+        return self.test_loader
---
+++ dataset/dataset.py
@@ -0,0 +1,35 @@
+import cv2
+import torch
+from torch.utils.data import Dataset
+import os
+from dataset.transformations import Transformations
+
+class BaseDataset(Dataset):
+    def __init__(self, image_dir, label_dir, transform=None):
+        """
+        Args:
+            image_dir (string): Directory with all the images.
+            label_dir (string): Directory with all the labels.
+            dataset_type (string): Type of dataset (e.g., 'cityscapes', 'nuscenes', 'ade20k').
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.transform = Transformations.get_transform(transform)
+        
+        self.images = sorted(os.listdir(image_dir))
+        self.labels = sorted(os.listdir(label_dir))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        sourceImage = cv2.imread(img_path, -1)
+        sourceImage = cv2.cvtColor(sourceImage, cv2.COLOR_BGR2RGB)
+        if self.transform:
+            sourceImage = self.transform(sourceImage)
+
+        label_path = os.path.join(self.label_dir, self.labels[idx])
+        labelImage = cv2.imread(label_path, -1)
+        labelImage = torch.from_numpy(labelImage).long()
+
+        return sourceImage, labelImage
